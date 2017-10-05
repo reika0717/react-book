cd0d9c1330efdcbbbd3dbcf6fd266dc4d78eac7e
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-10-05 21:43:39 +0900

    Add my first project made from create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+        <div>
+          <h1>Hello Word</h1>
+          <b>Bold</b>
+        </div>
+    )
+  }
+}
+
+//const App = () => <h1>Hello startless</h1>
+
+export default App
