a6d4ad46872318038dfe6c3a20b9ac48b67f89bb
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-10-06 10:43:51 +0900

    6. Use React Components as Children for Other Components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends React.Component {
     super();
     this.state = {
       txt: 'this is the state txt'
-      cat: 0
     }
   }
   update( e ){
@@ -15,14 +14,16 @@ class App extends React.Component {
 render(){
   return (
     <div>
-      <input type="text"
-      onChange={this.update.bind(this)}/>
-      <h1>{this.state.txt} - {this.state.cat}</h1>
+      <h1>{this.state.txt}</h1>
+      <Widget update={this.update.bind(this)} />
+      <Widget update={this.update.bind(this)} />
+      <Widget update={this.update.bind(this)} />
     </div>
   )
 }
 }
 
-
+const Widget = (props) =>
+<input type="text" onChange={props.update} />
 
 export default App
