04270624684f22d935430de9b756eed47a1232a7
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-10-06 10:51:01 +0900

    7. Access Nested Data with Reacts props.children

--- src/App.js
+++ src/App.js
@@ -2,28 +2,16 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      txt: 'this is the state txt'
-    }
-  }
-  update( e ){
-    this.setState({txt: e.target.value})
-  }
 render(){
-  return (
-    <div>
-      <h1>{this.state.txt}</h1>
-      <Widget update={this.update.bind(this)} />
-      <Widget update={this.update.bind(this)} />
-      <Widget update={this.update.bind(this)} />
-    </div>
-  )
+  return <Button>I <Heart />React</Button>
 }
 }
+const Button = (props) => <button>{props.children}</button>
 
-const Widget = (props) =>
-<input type="text" onChange={props.update} />
+class Heart extends React.Component {
+  render(){
+    return <span>&hearts;</span>
+  }
+}
 
 export default App
