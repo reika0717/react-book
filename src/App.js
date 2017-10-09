18f7c044a98a9efcfdffc5c58f27b19c45396725
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-10-09 21:32:05 +0900

    16. Build a JSX Live Complier as a React Component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
 
 import React from 'react';
-
-const HOC = {InnerComponent} => class extends React.Component {
-  constructor(){
-    super();
-    this.state = {count: 0}
-  }
-  update(){
-    this.setState({count: this.state.count + 1})
-  }
-  componentWillMount(){
-    console.log('will mount')
-  }
-  render(){
-    return(
-      <InnerComponent
-        {...this.props}
-        {...this.state}
-        update = {this.update.bind(this)}
-      />
-    )
-  }
-}
+import 'App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '/* add your jsx here */',
+      output: '',
+      err: ''
+    }
+  }
+  update(e){
+    let code = e.target.value;
+    try{
+      this.setState({
+        output: window.babel
+        .transform(code, { presets: ['es2015', 'react']})
+        .code,
+        err: ''
+      })
+    }
+    catch(err){
+      this.setState({err: err.message})
+    }
+  }
   render(){
     return (
       <div>
-        <button>button</button>
-        <hr />
-        <label>label</label>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea
+            onCange={this.update.bind(this)}
+            defaultValue={this.state.input}/>
+            <pre>
+              {this.state.output}
+            </pre>
+        </div>
       </div>
     )
   }
 }
-
-const Button = (props) =>
-<button onClick={props.update}>{props.children} - {props.count}</button>
-
-class Label extends React.Component{
-  componentWillMount(){
-    console.log('label will mount')
-  }
-  render(){
-    return(
-      <label onMouseMove={this.props.ipdate}>
-        {this.props.children} - {this.props.count}
-      </label>
-    )
-  }
-}
-
-const LabelHOC = HOC(Label)
 
 export default App
